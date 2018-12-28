644f73df30e49ec59e88e7c11e6fa2a95df8d8fb
Amelie Cheng <amelie1815@gmail.com>
2018-12-28 12:13:22 +0800

    update api

--- src/api.js
+++ src/api.js
@@ -1,23 +1,16 @@
 import axios from 'axios';
 
-const header = {
-  headers: {
-    'Client-ID': 'jdsl3lgf1c8gcxi44u29sm30m015n3'
-  }
-}
+const instance = axios.create({
+  baseURL: 'https://api.twitch.tv/helix/',
+  headers: { 'Client-ID': 'jdsl3lgf1c8gcxi44u29sm30m015n3' }
+});
 
-export const getGames = () => {
-  return axios.get('https://api.twitch.tv/helix/games/top?first=5', header);
-}
-
-export const getCurrentStreams = gameID => {
-  return axios.get(`https://api.twitch.tv/helix/streams?game_id=${gameID}&first=24`, header);
-}
-
+export const getGames = () => instance.get('/games/top?first=5');
+export const getCurrentStreams = gameID => instance.get(`/streams?game_id=${gameID}&first=24`);
 export const getUsers = userIds => {
-  let url = `https://api.twitch.tv/helix/users?id=${userIds[0]}`;
+  let url = `/users?id=${userIds[0]}`;
     for (let i = 1; i < userIds.length; i++) {
       url += `&id=${userIds[i]}`;
     };
-  return axios.get(url, header);
+  return instance.get(url);
 }
